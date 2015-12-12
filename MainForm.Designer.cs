eb9c2d3ecda8d9fe126eca9c2e746a3cd75ce9fc
Ivan K <to4ka.tut@bk.ru>
2015-12-12 21:03:36 +0400

    Initial commit

---
+++ MainForm.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace Urho3DSamplesLauncher
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.radioButtonAngelScript = new System.Windows.Forms.RadioButton();
+            this.radioButtonLua = new System.Windows.Forms.RadioButton();
+            this.listBoxScripts = new System.Windows.Forms.ListBox();
+            this.checkedListBoxOptions = new System.Windows.Forms.CheckedListBox();
+            this.radioButtonRelease = new System.Windows.Forms.RadioButton();
+            this.radioButtonDebug = new System.Windows.Forms.RadioButton();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.panelSelectSript = new System.Windows.Forms.Panel();
+            this.panelBuil = new System.Windows.Forms.Panel();
+            this.labelFileMask = new System.Windows.Forms.Label();
+            this.labelCommandLine = new System.Windows.Forms.Label();
+            this.comboBoxResolutions = new System.Windows.Forms.ComboBox();
+            this.panelSelectSript.SuspendLayout();
+            this.panelBuil.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // radioButtonAngelScript
+            // 
+            this.radioButtonAngelScript.AutoSize = true;
+            this.radioButtonAngelScript.Checked = true;
+            this.radioButtonAngelScript.Location = new System.Drawing.Point(0, 0);
+            this.radioButtonAngelScript.Name = "radioButtonAngelScript";
+            this.radioButtonAngelScript.Size = new System.Drawing.Size(79, 17);
+            this.radioButtonAngelScript.TabIndex = 0;
+            this.radioButtonAngelScript.TabStop = true;
+            this.radioButtonAngelScript.Text = "AngelScript";
+            this.radioButtonAngelScript.UseVisualStyleBackColor = true;
+            this.radioButtonAngelScript.CheckedChanged += new System.EventHandler(this.radioButtonAngelScript_CheckedChanged);
+            // 
+            // radioButtonLua
+            // 
+            this.radioButtonLua.AutoSize = true;
+            this.radioButtonLua.Location = new System.Drawing.Point(83, 0);
+            this.radioButtonLua.Name = "radioButtonLua";
+            this.radioButtonLua.Size = new System.Drawing.Size(43, 17);
+            this.radioButtonLua.TabIndex = 1;
+            this.radioButtonLua.Text = "Lua";
+            this.radioButtonLua.UseVisualStyleBackColor = true;
+            this.radioButtonLua.CheckedChanged += new System.EventHandler(this.radioButtonLua_CheckedChanged);
+            // 
+            // listBoxScripts
+            // 
+            this.listBoxScripts.FormattingEnabled = true;
+            this.listBoxScripts.Location = new System.Drawing.Point(12, 32);
+            this.listBoxScripts.Name = "listBoxScripts";
+            this.listBoxScripts.Size = new System.Drawing.Size(655, 173);
+            this.listBoxScripts.TabIndex = 3;
+            this.listBoxScripts.SelectedIndexChanged += new System.EventHandler(this.listBoxScripts_SelectedIndexChanged);
+            this.listBoxScripts.DoubleClick += new System.EventHandler(this.listBoxScripts_DoubleClick);
+            // 
+            // checkedListBoxOptions
+            // 
+            this.checkedListBoxOptions.CheckOnClick = true;
+            this.checkedListBoxOptions.FormattingEnabled = true;
+            this.checkedListBoxOptions.Location = new System.Drawing.Point(12, 211);
+            this.checkedListBoxOptions.Name = "checkedListBoxOptions";
+            this.checkedListBoxOptions.Size = new System.Drawing.Size(655, 64);
+            this.checkedListBoxOptions.TabIndex = 4;
+            this.checkedListBoxOptions.SelectedIndexChanged += new System.EventHandler(this.checkedListBoxOptions_SelectedIndexChanged);
+            // 
+            // radioButtonRelease
+            // 
+            this.radioButtonRelease.AutoSize = true;
+            this.radioButtonRelease.Checked = true;
+            this.radioButtonRelease.Location = new System.Drawing.Point(3, 1);
+            this.radioButtonRelease.Name = "radioButtonRelease";
+            this.radioButtonRelease.Size = new System.Drawing.Size(64, 17);
+            this.radioButtonRelease.TabIndex = 5;
+            this.radioButtonRelease.TabStop = true;
+            this.radioButtonRelease.Text = "Release";
+            this.radioButtonRelease.UseVisualStyleBackColor = true;
+            this.radioButtonRelease.CheckedChanged += new System.EventHandler(this.radioButtonRelease_CheckedChanged);
+            // 
+            // radioButtonDebug
+            // 
+            this.radioButtonDebug.AutoSize = true;
+            this.radioButtonDebug.Location = new System.Drawing.Point(73, 1);
+            this.radioButtonDebug.Name = "radioButtonDebug";
+            this.radioButtonDebug.Size = new System.Drawing.Size(57, 17);
+            this.radioButtonDebug.TabIndex = 6;
+            this.radioButtonDebug.Text = "Debug";
+            this.radioButtonDebug.UseVisualStyleBackColor = true;
+            this.radioButtonDebug.CheckedChanged += new System.EventHandler(this.radioButtonDebug_CheckedChanged);
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(592, 281);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(75, 51);
+            this.buttonStart.TabIndex = 8;
+            this.buttonStart.Text = "Start";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // panelSelectSript
+            // 
+            this.panelSelectSript.Controls.Add(this.radioButtonAngelScript);
+            this.panelSelectSript.Controls.Add(this.radioButtonLua);
+            this.panelSelectSript.Location = new System.Drawing.Point(12, 9);
+            this.panelSelectSript.Name = "panelSelectSript";
+            this.panelSelectSript.Size = new System.Drawing.Size(130, 18);
+            this.panelSelectSript.TabIndex = 0;
+            // 
+            // panelBuil
+            // 
+            this.panelBuil.Controls.Add(this.radioButtonRelease);
+            this.panelBuil.Controls.Add(this.radioButtonDebug);
+            this.panelBuil.Location = new System.Drawing.Point(238, 281);
+            this.panelBuil.Name = "panelBuil";
+            this.panelBuil.Size = new System.Drawing.Size(200, 21);
+            this.panelBuil.TabIndex = 6;
+            // 
+            // labelFileMask
+            // 
+            this.labelFileMask.AutoSize = true;
+            this.labelFileMask.BackColor = System.Drawing.SystemColors.Control;
+            this.labelFileMask.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.labelFileMask.Location = new System.Drawing.Point(144, 9);
+            this.labelFileMask.Name = "labelFileMask";
+            this.labelFileMask.Size = new System.Drawing.Size(73, 15);
+            this.labelFileMask.TabIndex = 9;
+            this.labelFileMask.Text = "labelFileMask";
+            // 
+            // labelCommandLine
+            // 
+            this.labelCommandLine.AutoSize = true;
+            this.labelCommandLine.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.labelCommandLine.Location = new System.Drawing.Point(12, 308);
+            this.labelCommandLine.Name = "labelCommandLine";
+            this.labelCommandLine.Size = new System.Drawing.Size(98, 15);
+            this.labelCommandLine.TabIndex = 10;
+            this.labelCommandLine.Text = "labelCommandLine";
+            // 
+            // comboBoxResolutions
+            // 
+            this.comboBoxResolutions.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxResolutions.FormattingEnabled = true;
+            this.comboBoxResolutions.Location = new System.Drawing.Point(12, 281);
+            this.comboBoxResolutions.Name = "comboBoxResolutions";
+            this.comboBoxResolutions.Size = new System.Drawing.Size(220, 21);
+            this.comboBoxResolutions.TabIndex = 5;
+            this.comboBoxResolutions.SelectedIndexChanged += new System.EventHandler(this.comboBoxResolutions_SelectedIndexChanged);
+            // 
+            // MainForm
+            // 
+            this.AcceptButton = this.buttonStart;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(679, 346);
+            this.Controls.Add(this.comboBoxResolutions);
+            this.Controls.Add(this.labelCommandLine);
+            this.Controls.Add(this.labelFileMask);
+            this.Controls.Add(this.panelBuil);
+            this.Controls.Add(this.panelSelectSript);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.checkedListBoxOptions);
+            this.Controls.Add(this.listBoxScripts);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Urho3D Samples Launcher";
+            this.panelSelectSript.ResumeLayout(false);
+            this.panelSelectSript.PerformLayout();
+            this.panelBuil.ResumeLayout(false);
+            this.panelBuil.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton radioButtonAngelScript;
+        private System.Windows.Forms.RadioButton radioButtonLua;
+        private System.Windows.Forms.ListBox listBoxScripts;
+        private System.Windows.Forms.CheckedListBox checkedListBoxOptions;
+        private System.Windows.Forms.RadioButton radioButtonRelease;
+        private System.Windows.Forms.RadioButton radioButtonDebug;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Panel panelSelectSript;
+        private System.Windows.Forms.Panel panelBuil;
+        private System.Windows.Forms.Label labelFileMask;
+        private System.Windows.Forms.Label labelCommandLine;
+        private System.Windows.Forms.ComboBox comboBoxResolutions;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+using System.Management;
+
+namespace Urho3DSamplesLauncher
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            CreateResolutionsList();
+            InitOptionsList();
+            RefreshFileListAndMaskLabel();
+        }
+
+        Dictionary<string, string> options = new Dictionary<string, string>();
+
+        private void InitOptionsList()
+        {
+            string name, value;
+
+            name = "Start in windowed mode";
+            value = " -w";
+            checkedListBoxOptions.Items.Add(name);
+            options.Add(name, value);
+
+            name = "Enable resizing when in windowed mode";
+            value = " -s";
+            checkedListBoxOptions.Items.Add(name);
+            options.Add(name, value);
+
+            name = "Force OpenGL 2 use even if OpenGL 3 is available";
+            value = " -gl2";
+            checkedListBoxOptions.Items.Add(name);
+            options.Add(name, value);
+
+            name = "Disable frame limiter";
+            value = " -nolimit";
+            checkedListBoxOptions.Items.Add(name);
+            options.Add(name, value);
+
+            name = "Disable shadow rendering";
+            value = " -noshadows";
+            checkedListBoxOptions.Items.Add(name);
+            options.Add(name, value);
+
+            name = "Use light pre-pass rendering";
+            value = " -prepass";
+            checkedListBoxOptions.Items.Add(name);
+            options.Add(name, value);
+
+            name = "Use deferred rendering";
+            value = " -deferred";
+            checkedListBoxOptions.Items.Add(name);
+            options.Add(name, value);
+        }
+
+        private void radioButtonAngelScript_CheckedChanged(object sender, EventArgs e)
+        {
+            RefreshFileListAndMaskLabel();
+        }
+
+        private void radioButtonLua_CheckedChanged(object sender, EventArgs e)
+        {
+            RefreshFileListAndMaskLabel();
+        }
+
+        private void RefreshFileListAndMaskLabel()
+        {
+            string dir, mask;
+
+            if (radioButtonAngelScript.Checked)
+            {
+                dir = "Data\\Scripts";
+                mask = "*_*.as";
+                labelFileMask.Text = "Data\\Scripts\\*_*.as";
+            }
+            else
+            {
+                dir = "Data\\LuaScripts";
+                mask = "*_*.lua";
+                labelFileMask.Text = "Data\\LuaScripts\\*_*.lua";
+            }
+
+            listBoxScripts.Items.Clear();
+
+            try
+            {
+                listBoxScripts.Items.AddRange(Directory.GetFiles(dir, mask));
+                listBoxScripts.SelectedIndex = 0; // it calls UpdateCommandLineLabel();
+            }
+            catch
+            {
+                MessageBox.Show("Scripts not found", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void StartPlayer()
+        {
+            try
+            {
+                Process.Start(GetPlayerName(), GetArguments());
+            }
+            catch
+            {
+                MessageBox.Show("Player not found", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            StartPlayer();
+        }
+
+        private void listBoxScripts_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            UpdateCommandLineLabel();
+        }
+
+        private void UpdateCommandLineLabel()
+        {
+            if (comboBoxResolutions.Items.Count == 0 || listBoxScripts.Items.Count == 0) // before initialisation
+                return;
+
+            string result = GetPlayerName() + " " + GetArguments();
+            labelCommandLine.Text = result;
+        }
+
+        string GetPlayerName()
+        {
+            string result = "Urho3DPlayer_d.exe";
+
+            if (radioButtonRelease.Checked)
+                result = "Urho3DPlayer.exe";
+
+            return result;
+        }
+
+        string GetArguments()
+        {
+            string result = listBoxScripts.SelectedItem.ToString();
+            
+            if (comboBoxResolutions.SelectedItem.ToString() != "Use current resolution")
+                result += resolutions[comboBoxResolutions.SelectedItem.ToString()];
+
+            foreach (var item in checkedListBoxOptions.CheckedItems)
+                result += options[item.ToString()];
+
+            return result;
+        }
+
+        private void checkedListBoxOptions_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            UpdateCommandLineLabel();
+        }
+
+        private void radioButtonRelease_CheckedChanged(object sender, EventArgs e)
+        {
+            UpdateCommandLineLabel();
+        }
+
+        private void radioButtonDebug_CheckedChanged(object sender, EventArgs e)
+        {
+            UpdateCommandLineLabel();
+        }
+
+        private void listBoxScripts_DoubleClick(object sender, EventArgs e)
+        {
+            StartPlayer();
+        }
+
+        Dictionary<string, string> resolutions = new Dictionary<string, string>();
+
+        private void CreateResolutionsList()
+        {
+            var scope = new ManagementScope();
+            var query = new ObjectQuery("SELECT * FROM CIM_VideoControllerResolution");
+
+            using (var searcher = new ManagementObjectSearcher(scope, query))
+            {
+                var results = searcher.Get();
+
+                comboBoxResolutions.Items.Add("Use current resolution");
+
+                foreach (var result in results)
+                {
+                    string name = result["HorizontalResolution"].ToString() + "x" + result["VerticalResolution"];
+                    string value = " -x " + result["HorizontalResolution"].ToString() + " -y " + result["VerticalResolution"];
+                    if (!comboBoxResolutions.Items.Contains(name))
+                    {
+                        comboBoxResolutions.Items.Add(name);
+                        resolutions[name] = value;
+                    }
+                }
+            }
+
+            comboBoxResolutions.SelectedIndex = 0; // it calls UpdateCommandLineLabel();
+        }
+
+        private void comboBoxResolutions_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            UpdateCommandLineLabel();
+        }
+    }
+}
